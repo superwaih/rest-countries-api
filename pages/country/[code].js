7e625d77d19e0c5298ac5a51a215038e5acdf0bf
shittu adewale <adewale.shittu@milsat.tech>
2023-06-19 09:22:32 +0100

    fixed errors

--- pages/country/[code].js
+++ pages/country/[code].js
@@ -1,99 +1,143 @@
-import { useRouter } from 'next/router';
-import { Link } from 'next/link'
-import { BiArrowBack, BiListUl } from 'react-icons/bi';
-
+import { useRouter } from "next/router";
+import { Link } from "next/link";
+import { BiArrowBack, BiListUl } from "react-icons/bi";
+import Image from "next/image";
 export const getStaticPaths = async () => {
-  const res = await fetch('https://restcountries.com/v2/all');
+  const res = await fetch("https://restcountries.com/v2/all");
   const data = await res.json();
 
   // map data to an array of path objects with params (id)
-  const paths = data.map(county => {
+  const paths = data.map((county) => {
     return {
-      params: { code: county.alpha3Code.toString() }
-    }
-  })
+      params: { code: county?.alpha3Code?.toString() },
+    };
+  });
 
   return {
     paths,
-    fallback: false
-  }
-}
-
+    fallback: false,
+  };
+};
 
 export const getStaticProps = async (context) => {
   const count = context.params.code;
-  const borderArr = []
-  const res = await fetch('https://restcountries.com/v2/all');
+  const borderArr = [];
+  const res = await fetch("https://restcountries.com/v2/all");
   const data = await res.json();
-  
- 
-  const singleCountry = data.filter(single => single.alpha3Code === count);
-  
+
+  const singleCountry = data.filter((single) => single?.alpha3Code === count);
+
   // const borderCountriesArr = singleCountry.map((e) => {
   //   borderArr.push(e.borders)
   // })
   return {
-    props: { country: singleCountry}
-
-  }
-}
-
-
-
-
-
+    props: { country: singleCountry },
+  };
+};
 
 const CountryDetail = ({ country, world, border }) => {
-  
-
-  const router = useRouter()
+  const router = useRouter();
   const handleBack = () => {
-    router.back()
-  }
+    router.back();
+  };
 
   return (
     <div>
-      <div className="back-btn  p-6" >
-        <button className='shadow flex items-center dark:bg-DarkModeElement rounded text-[13px] p-3' onClick={handleBack}>
+      <div className="back-btn  p-6">
+        <button
+          className="shadow flex items-center dark:bg-DarkModeElement rounded text-[13px] p-3"
+          onClick={handleBack}
+        >
           <BiArrowBack />
-          Back</button>
+          Back
+        </button>
       </div>
 
       {country.map((data, i) => {
         return (
-          <div key={i} className='country-detail flex flex-col lg:flex-row  space-y-5 p-6'>
+          <div
+            key={i}
+            className="country-detail flex flex-col lg:flex-row  space-y-5 p-6"
+          >
             <div className="country-flag md:w-4/5 md:h-80 lg:w-3/5 ">
-              <img src={data.flag} alt="country flag" />
+              <Image
+                width={500}
+                height={400}
+                objectFit="cover"
+                src={data.flag}
+                alt="country flag"
+              />
             </div>
 
             <div className="country-stats lg:pl-20 space-y-5 2/5">
-              <h1 className='font-bold text-xl'>{data.name}</h1>
+              <h1 className="font-bold text-xl">{data.name}</h1>
               <div className="statsBox flex flex-col md:flex-row ">
                 <div className="native space-y-2 text-[14px]">
-                  <p>Native Name: <span>{data.nativeName}</span></p>
-                  <p className='font-semibold' >Population: <span className='dark:text-gray-300 font-light'>{data.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</span></p>
-                  <p className='font-semibold' >Region: <span className='dark:text-gray-300 font-light'>{data.region}</span></p>
-                  <p className='font-semibold' >Sub Region: <span className='dark:text-gray-300 font-light' >{data.subregion}</span></p>
-                  <p className='font-semibold' >Capital: <span className='dark:text-gray-300 font-light' >{data.capital}</span></p>
+                  <p>
+                    Native Name: <span>{data.nativeName}</span>
+                  </p>
+                  <p className="font-semibold">
+                    Population:{" "}
+                    <span className="dark:text-gray-300 font-light">
+                      {data.population
+                        .toString()
+                        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                    </span>
+                  </p>
+                  <p className="font-semibold">
+                    Region:{" "}
+                    <span className="dark:text-gray-300 font-light">
+                      {data.region}
+                    </span>
+                  </p>
+                  <p className="font-semibold">
+                    Sub Region:{" "}
+                    <span className="dark:text-gray-300 font-light">
+                      {data.subregion}
+                    </span>
+                  </p>
+                  <p className="font-semibold">
+                    Capital:{" "}
+                    <span className="dark:text-gray-300 font-light">
+                      {data.capital}
+                    </span>
+                  </p>
                 </div>
 
                 <div className="otherstats md:pl-5 text-[14px]">
-                  <p className='font-semibold'>Top Level Domain: <span className='dark:text-gray-300 font-light' >{data.capital}</span></p>
-                  <p className='font-semibold' >Currencies: <span className='dark:text-gray-300 font-light' >{data.currencies[0].name}</span></p>
-                  <p className='font-semibold'>Languages:
+                  <p className="font-semibold">
+                    Top Level Domain:{" "}
+                    <span className="dark:text-gray-300 font-light">
+                      {data.capital}
+                    </span>
+                  </p>
+                  <p className="font-semibold">
+                    Currencies:{" "}
+                    <span className="dark:text-gray-300 font-light">
+                      {data.currencies[0].name}
+                    </span>
+                  </p>
+                  <p className="font-semibold">
+                    Languages:
                     {data.languages.map((lang, i) => {
                       return (
-                        <span className='dark:text-gray-300 font-extralight' key={i} > {lang.name} </span>
-                      )
+                        <span
+                          className="dark:text-gray-300 font-extralight"
+                          key={i}
+                        >
+                          {" "}
+                          {lang.name}{" "}
+                        </span>
+                      );
                     })}
                   </p>
                 </div>
               </div>
 
               <div className="border-countries text-[13px] flex space-x-2 justify-between">
-                <h3 className='font-semibold' >Border Countries: </h3>
-                <div className='grid-cols-2 md:grid-cols-4 grid'>
-                {/* {borderCountry.map((e) => { 
+                <h3 className="font-semibold">Border Countries: </h3>
+                <div className="grid-cols-2 md:grid-cols-4 grid">
+                  {/* {borderCountry.map((e) => { 
                   return(
                     <a href={'/country/'+ e.alpha3Code} className='p-2 hover:opacity-40 cursor-pointer ' key={e.alpha3Code} >
                         <span className='rounded shadow dark:bg-DarkModeElement p-2 '>{e.name.length > 15 ? e.name.slice(0,8)  : e.name  }</span>
@@ -104,16 +148,10 @@ const CountryDetail = ({ country, world, border }) => {
               </div>
             </div>
           </div>
-        )
+        );
       })}
-
-
-      
-
-
-
     </div>
   );
-}
+};
 
 export default CountryDetail;
